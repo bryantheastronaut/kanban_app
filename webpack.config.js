fbfc3851126b487bfc72a3e84d0e52acac0d2349
bryantheastronaut <bgilbraith12@gmail.com>
2016-03-28 22:33:18 -0700

    Finish setting up Webpack dev env

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
+const webpack = require('webpack');
 const path = require('path');
-
 const merge = require('webpack-merge');
+
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
 
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
@@ -17,12 +19,49 @@ const common = {
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        //Test expects a RegEx!
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        //include accepts either a path or array of paths
+        include: PATHS.app
+      }
+    ]
   }
 };
 
 //Default configs
 if (TARGET === 'start' || !TARGET) {
-  module.exports = merge(common, {});
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      contentBase: PATHS.build,
+
+      //Enable history API fallback so HTML5 History API based
+      //routing works. This is a good default that will come in
+      //handy in more complicated setups.
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      //display only errors to reduce amount of output
+      stats: 'errors-only',
+
+      //parse host and port from env so this is easy to customeize
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new NpmInstallPlugin({
+        save: true //--save
+      })
+    ]
+  });
 }
 
 if (TARGET === 'build') {
