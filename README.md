dec1c86b41ffba98c9fe455ec2a85d0f4e28adaa
bryantheastronaut <bgilbraith12@gmail.com>
2016-03-28 20:35:45 -0700

    Set up webpack and add README file

---
+++ README.md
@@ -0,0 +1,5 @@
+Kanban app
+=====
+
+This is a basic todo app/kanban that is sourced from the
+SurviveJS book. All credit goes there.
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+const merge = require('webpack-merge');
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+const common = {
+  // Entry accepts a path or an object of entries. We'll be using the
+  //latter form given its convienent with more complex configs.
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  }
+};
+
+//Default configs
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {});
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {});
+}
